ee36a582cceadee1ec4d739f46d43cfe56115680
Jackson Hochscheidt <jackson94h@gmail.com>
2015-07-30 14:59:53 -0300

    faz leitura do arquivo para um buffer

--- Cifra de Cesar/cifraDeCesar.java
+++ Cifra de Cesar/cifraDeCesar.java
@@ -1,12 +1,30 @@
-public class cifraDeCesar{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+
+public class cifraDeCesar{
+	
     public static void main(String[] args){
 
-        String url = 
-
-
-
-    }
+    	
+        String url = "textoPuro.txt" ; // texto a ser cifrado/decifrado
+        File arquivoTexto =  new File(url);
+        File arquivoTextoCifrado = new File("cifrado.txt");
+        
+        try{
+        	arquivoTextoCifrado.createNewFile();
+        	final int chave = 5;
+        	FileReader ler = new FileReader(arquivoTexto); // arquivo de leitura
+        	BufferedReader lerBuffer = new BufferedReader(ler); // buffer de leitura
+        	
+        }catch(IOException e){
+        	System.out.println("nao foi possivel criar cifrado.txt");
+        }
+        
+        
+     }
 
 
 }
