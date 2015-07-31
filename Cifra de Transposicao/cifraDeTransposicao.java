9f4367223e138780b29a38b7af0c82953bd5c202
Jackson Hochscheidt <jackson94h@gmail.com>
2015-07-31 17:35:15 -0300

    cifrando texto original e escrevendo em arquivo arquivoCifrado.txt

--- Cifra de Transposicao/cifraDeTransposicao.java
+++ Cifra de Transposicao/cifraDeTransposicao.java
@@ -6,6 +6,7 @@ public class cifraDeTransposicao{
 	public static void main(String[] args){
 		
 		File arquivoOriginal = new File("textoPuro.txt");
+		File arquivoCifrado = new File("arquivoCifrado.txt");
 		long tamanhoArq;
 		tamanhoArq = arquivoOriginal.length();
 		//System.out.println(tamanhoArq);
@@ -25,24 +26,23 @@ public class cifraDeTransposicao{
 	
 			while(linha != null){
 				tamLinha = linha.length();
-	
 				for(int i = 0; i < row;  i++){
 					for(int j = 0; j < col; j++){
 						if(posicaoLinha == tamLinha){
 							linha = lerBuffer.readLine();
 							if(linha == null){
-								for(; i < row; i++){
+								/*for(; i < row; i++){
 									for(; j < col; j++){
 										matrizTransposicao[j][i] = 'x';
 									}
-								}
+								}*/
+								i = row;
+								j = col;
 								break;
-							}else{
-								tamLinha = linha.length();
-								posicaoLinha = 0;
 							}
-							
-						}
+							tamLinha = linha.length();
+							posicaoLinha = 0;
+						}	
 						if(posicaoLinha < tamLinha){
 							matrizTransposicao[j][i] = linha.charAt(posicaoLinha);
 							posicaoLinha++;
@@ -50,11 +50,20 @@ public class cifraDeTransposicao{
 					}
 				}
 			}
+			arquivoCifrado.createNewFile();
+			FileWriter escrever = new FileWriter(arquivoCifrado);
+			BufferedWriter escreverBuffer = new BufferedWriter(escrever);
+			for(int i = 0; i < tamanhoMatrizQuadrada;i++){
+				for(int j = 0; j < tamanhoMatrizQuadrada; j++){
+					escrever.write(matrizTransposicao[i][j]);
+				}
+			}
+			escreverBuffer.close();
+			escrever.close();
 		}catch(IOException e){
 			System.out.println("erro de arquivo");
 		}
-		
-		
+		// imprime a matriz de char, para fins de teste, para verificar se esta correta a escrita na matriz
 		for(int i = 0; i < row; i++){
 			for(int j = 0; j < col; j++){
 				System.out.print(" " + matrizTransposicao[i][j]);
