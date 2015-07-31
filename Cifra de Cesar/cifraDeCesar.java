464611b8fb63cdb119abf0d01cdb03fdd965e281
Jackson Hochscheidt <jackson94h@gmail.com>
2015-07-31 15:41:11 -0300

    teste

--- Cifra de Cesar/cifraDeCesar.java
+++ Cifra de Cesar/cifraDeCesar.java
@@ -15,11 +15,12 @@ public class cifraDeCesar{
         
         File arquivoTextoOriginal =  new File("textoPuro.txt"); // texto a ser cifrado/decifrado
         File arquivoTextoCifrado = new File("cifrado.txt"); // texto apos cifrado
-        arquivoTextoCifrado.delete();
-        arquivoTextoCifrado = new File("cifrado.txt"); 
         File arquivoTextoDecifrado = new File("decifrado.txt"); // texto apos desifrado, deve estar igual ao original
         
         try{
+        	
+        	
+       	// parte que cifra o texto
         	arquivoTextoCifrado.createNewFile();
         	FileReader ler = new FileReader(arquivoTextoOriginal); // arquivo de leitura
         	BufferedReader lerBuffer = new BufferedReader(ler); // buffer de leitura
@@ -29,9 +30,33 @@ public class cifraDeCesar{
         	int valor ;
         	while(linha != null){
         		char[] aux = new char[linha.length()];
+        		// passa a string LINHA para dentro de um vetor de char para poder substituir os caracteres
         		for(int cont = 0; cont < linha.length(); cont++){
         			aux[cont] = linha.charAt(cont);
         		}        		 
+        		// percorre o vetor de char trocando (cifrando) o texto
+        		for(int i = 0; i < aux.length; i++){
+        			valor = ((int)aux[i] + chave) % 256 ;
+        			aux[i] = (char) valor;
+        			escrever.write(aux[i]);
+        		}
+        		linha = lerBuffer.readLine();
+        	}
+        	escreverBuffer.close();
+        	escrever.close();
+        	
+        	// parte de decifra o texto
+        	arquivoTextoDecifrado.createNewFile();
+        	ler = new FileReader(arquivoTextoCifrado);
+        	escrever = new FileWriter(arquivoTextoDecifrado, true);
+        	linha = lerBuffer();
+        	while(linha != null){
+        		char[] aux = new char[linha.length()];
+        		// passa a string LINHA para dentro de um vetor de char para poder substituir os caracteres
+        		for(int cont = 0; cont < linha.length(); cont++){
+        			aux[cont] = linha.charAt(cont);
+        		}        		 
+        		// percorre o vetor de char trocando (cifrando) o texto
         		for(int i = 0; i < aux.length; i++){
         			valor = ((int)aux[i] + chave) % 256 ;
         			aux[i] = (char) valor;
@@ -42,11 +67,7 @@ public class cifraDeCesar{
         	escreverBuffer.close();
         	escrever.close();
     	}catch(IOException e){
-        	System.out.println("nao foi possivel criar cifrado.txt");
+        	System.out.println("Erro na criação, escrita ou leitura de algum arquivo!!!");
         }
-        
-        
      }
-
-
 }
