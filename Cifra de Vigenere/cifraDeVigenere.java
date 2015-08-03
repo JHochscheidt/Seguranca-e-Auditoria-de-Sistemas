7e84383e9ec32187060b77c990d9fbe66fac2ba8
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-03 18:58:04 -0300

    nada

--- Cifra de Vigenere/cifraDeVigenere.java
+++ Cifra de Vigenere/cifraDeVigenere.java
@@ -5,20 +5,29 @@ public class cifraDeVigenere{
 	public static void main(String[] args){
 		
 		char[] chave = {'V','I','G','E','N','E','R','E'};
-		
+		File arquivoOriginal = new File("textoPuro.txt");
+		File textoCifrado = new File("textoCifrado.txt");
+		File textoDecifrado = new File("textoDecifrado.txt");
 		
 		
 		try{
-			File arquivoOriginal = new File("textoPuro.txt");
+			
 		
 			FileReader ler = new FileReader(arquivoOriginal);
 			BufferedReader lerBuffer = new BufferedReader(ler);
-				
+			
+		// cifrando o texto
 			String linha = lerBuffer.readLine();
 			while(linha != null){
-				System.out.println(linha);
+				
+				
+				
 				linha = lerBuffer.readLine();
 			}
+			
+			
+			
+			
 		}catch(IOException e){
 			System.out.println("erro na leitura/escrita do arquivo");
 		}
