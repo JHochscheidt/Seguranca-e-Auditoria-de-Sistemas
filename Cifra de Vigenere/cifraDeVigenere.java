0eb243a8336c180ec3185ea596be8dc083c6981e
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-03 18:53:51 -0300

    definindo a chave como VIGENERE

--- Cifra de Vigenere/cifraDeVigenere.java
+++ Cifra de Vigenere/cifraDeVigenere.java
@@ -1,6 +1,27 @@
+import java.io.*;
+
 public class cifraDeVigenere{
 	
 	public static void main(String[] args){
+		
+		char[] chave = {'V','I','G','E','N','E','R','E'};
+		
+		
+		
+		try{
+			File arquivoOriginal = new File("textoPuro.txt");
+		
+			FileReader ler = new FileReader(arquivoOriginal);
+			BufferedReader lerBuffer = new BufferedReader(ler);
+				
+			String linha = lerBuffer.readLine();
+			while(linha != null){
+				System.out.println(linha);
+				linha = lerBuffer.readLine();
+			}
+		}catch(IOException e){
+			System.out.println("erro na leitura/escrita do arquivo");
+		}
 		
 		
 		
