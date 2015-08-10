aa0941d2131498740b7e712f3053bbb61fd0bc7e
Jackson Hochscheidt <jackson94h@gmail.com>
2015-08-10 17:11:37 -0300

    feito codigo para criacao de uma tabela com os respectivos valores em char dos valores inteiros, e a cifra do arquivo esta funcionando. Falta agora decifrar

--- Cifra de Substituicao/cifraDeSubstituicao.java
+++ Cifra de Substituicao/cifraDeSubstituicao.java
@@ -1,86 +1,97 @@
 import java.io.*;
 public class cifraDeSubstituicao{
-	
+
 	public static void main(String[] args){
-		
-		
-		
-		
+
 		// criando arquivo com a tabela de codigos
 		File tabela = new File("tabela.txt");
+		File tabelaChar = new File("tabelaChar.txt");
+		// verifica se tabela ja existe, caso nao exista, cria tabela
+		
 		int tamTabela = 256;
 		try{
-			tabela.createNewFile();	
-			FileWriter escreverTabela = new FileWriter(tabela);
-			BufferedWriter bufferTabela = new BufferedWriter(escreverTabela);
-			int posicao = tamTabela-1;
-			for(int i = 0; i < tamTabela; i++){
-				if(i%2 == 0){
-					escreverTabela.write( i + " " + i + "\n");
-				}else{
-					escreverTabela.write( i + " " +  ((i+2)%tamTabela) + "\n");
-				}					
-			}
-			bufferTabela.close();
-			escreverTabela.close();		
-		}catch(IOException e){
-			System.out.println("erro na leitura,escrita ou criacao de algum arquivo");
-		}
-		
-		/*
-		 try{
-			FileReader lerTabela = new FileReader(tabela);	
-			BufferedReader bufferTabela = new BufferedReader(lerTabela);
-		
-			String linha = bufferTabela.readLine();
-			while(linha != null){
-				System.out.println(linha + "\t" + linha.length());
-				linha = bufferTabela.readLine();
+			tabela.createNewFile();
+			tabelaChar.createNewFile();
+			if(!tabela.exists() || !tabelaChar.exists()){	
+				FileWriter escreverTabela = new FileWriter(tabela);
+				FileWriter escreverTabelaChar = new FileWriter(tabelaChar);
 				
+				BufferedWriter bufferTabela = new BufferedWriter(escreverTabela);
+				BufferedWriter bufferTabelaChar = new BufferedWriter(escreverTabelaChar);
+				int posicao = tamTabela-1;
+				char charI;
+				char modCharI;
+				int mod;
+				for(int i = 0; i < tamTabela; i++){
+					charI = (char) i;
+					mod = ((i+2)%tamTabela);
+					modCharI = (char) mod;
+					System.out.println("valor i -->" + i + "valor charI --> " + charI + " valor modCharI -->" + modCharI + " valor mod -->" + mod);
+					if(i%2 == 0){
+						escreverTabela.write( i + " " + i + "\n");
+						escreverTabelaChar.write(charI + " " + charI + "\n");
+					}else{
+						escreverTabela.write( i + " " +  ((i+2)%tamTabela) + "\n");
+						escreverTabelaChar.write( charI + " " + modCharI + "\n");
+					}
+				}
+				bufferTabela.close();
+				bufferTabelaChar.close();
+				escreverTabela.close();
+				escreverTabelaChar.close();				
 			}
 		}catch(IOException e){
-				System.out.println("erro no arquivo");
+				System.out.println("erro na leitura,escrita ou criacao de algum arquivo");
 		}
-		*/		
+			
 		
-		
-		// cifrando arquivo fonte original
-		System.out.println("Cifrando arquivo...");	
-		File arquivoFonte = new File("textoPuro.txt");
 		
 		try{
+			
+			// cifrando arquivo fonte original
+			System.out.println("Cifrando arquivo...");
+			File arquivoFonte = new File("textoPuro.txt");
+			
 			FileReader lerFonte = new FileReader(arquivoFonte);
 			BufferedReader bufferFonte = new BufferedReader(lerFonte);
-			
-			FileReader lerTabela = new FileReader(tabela);
-			BufferedReader bufferTabela = new BufferedReader(lerTabela);
-			
+						
+			FileReader lerTabela;
+			BufferedReader bufferTabela;
+			String linhaTabela;
 			
 			File cifrado = new File("cifrado.txt");
 			cifrado.createNewFile();
 			FileWriter escreverTextoCifrado = new FileWriter(cifrado);
 			BufferedWriter bufferTextoCifrado = new BufferedWriter(escreverTextoCifrado);
-			
-			String linha = bufferFonte.readLine();
-			String linhaTabela;
-			int valorC;
-			while(linha != null){
-				//	System.out.print("Linha");
-				for(int i = 0; i < linha.length(); i++){
-					//System.out.print("Caracter");
-					valorC = (int)linha.charAt(i);
-					System.out.print(valorC);
-					linhaTabela = bufferTabela.readLine();
+
+			String linhaTextoFonte = bufferFonte.readLine();
 					
+			int valorIntC;
+			// enquanto tiver linhas no arquivo original
+			while(linhaTextoFonte != null){
+				//percorre a linha atual
+				for(int i = 0; i < linhaTextoFonte.length(); i++){
+					valorIntC = (int)linhaTextoFonte.charAt(i);
+					//System.out.print(valorC -> linhaTextoFonte.charAt(i));
+					
+					lerTabela = new FileReader(tabela);
+					bufferTabela = new BufferedReader(lerTabela);
+					linhaTabela = bufferTabela.readLine();
+					//System.out.print(linhaTabela + "-");
+					
+					String[] aux = new String[2];
+					int  intC;
+					char charC;
 					while(linhaTabela != null){
-						//System.out.print("LinhaTabela");
-						String[] aux = new String[2];
-						aux = linhaTabela.split(" ");
-						int valorAux = Integer.parseInt(aux[0]);
-						if(valorC == valorAux){
-							System.out.println("aqui");
+						aux = linhaTabela.split(" "); // separa a linha por espacos
+						//System.out.println(aux[0] + " " + aux[1]);	
+						intC = Integer.parseInt(aux[0]); // pega a primeira "string" da linha AUX
+						//System.out.println(intC + " " + aux[0]);
+						//System.out.println(valorC + " " + aux + " " + valorCchar );
+						if(valorIntC == intC){
 							int a = Integer.parseInt(aux[1]);
 							char c = (char) a;
+							//System.out.println(c + " " + a);
 							escreverTextoCifrado.write(c);
 							break;
 						}else{
@@ -89,14 +100,18 @@ public class cifraDeSubstituicao{
 						// for(int pos = 0; pos < aux.length; pos++){System.out.print(aux[pos]);} // teste de verificacao dos valores de aux
 					}
 				}
-				linha = bufferFonte.readLine();		
+				linhaTextoFonte = bufferFonte.readLine();
 			}
 			bufferTextoCifrado.close();
-			escreverTextoCifrado.close();					
-
+			escreverTextoCifrado.close();
+			
+			
+			// decifrando arquivo cifrado
+			
+			
 			
 		}catch(IOException e){
 			System.out.println("erro no arquivo");
-		}				
+		}
 	}
 }
